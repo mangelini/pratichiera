51b867cc0f8a773deb3d9c51b09f1dd425002193
dev <matteo.angelini@ploarity.dev>
2025-02-01 16:50:38 +0000

    hero wip

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -2,6 +2,7 @@ import { publicUrl } from "@/env.mjs";
 import { getTranslations } from "@/i18n/server";
 import StoreConfig from "@/store.config";
 import { CategoryBox } from "@/ui/category-box";
+import Hero from "@/ui/hero";
 import { ProductList } from "@/ui/products/product-list";
 import { SeoH1 } from "@/ui/seo-h1";
 import { YnsLink } from "@/ui/yns-link";
@@ -19,36 +20,8 @@ export default async function Home() {
 
   return (
     <main>
-      <section className="rounded bg-neutral-100 py-8 sm:py-12">
-        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
-          <div className="max-w-md space-y-4">
-            <h2 className="text-balance text-3xl font-bold tracking-tight md:text-4xl">
-              {t("hero.title")}
-            </h2>
-            <p className="text-pretty text-neutral-600">
-              {t("hero.description")}
-            </p>
-            <YnsLink
-              className="inline-flex h-10 items-center justify-center rounded-full bg-neutral-900 px-6 font-medium text-neutral-50 transition-colors hover:bg-neutral-900/90 focus:outline-hidden focus:ring-1 focus:ring-neutral-950"
-              href={t("hero.link")}
-            >
-              {t("hero.action")}
-            </YnsLink>
-          </div>
-          <Image
-            alt="Cup of Coffee"
-            loading="eager"
-            priority={true}
-            className="rounded"
-            height={450}
-            width={450}
-            src="https://files.stripe.com/links/MDB8YWNjdF8xT3BaeG5GSmNWbVh6bURsfGZsX3Rlc3RfaDVvWXowdU9ZbWlobUIyaHpNc1hCeDM200NBzvUjqP"
-            style={{
-              objectFit: "cover",
-            }}
-            sizes="(max-width: 640px) 70vw, 450px"
-          />
-        </div>
+      <section className="flex w-full rounded">
+        <Hero />
       </section>
 
       <SeoH1 className="text-4xl md:text-6xl mt-9 font-serif font-normal leading-[120%]">
---
+++ src/ui/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <video
+      autoPlay
+      className="object-cover rounded-lg h-fit w-full"
+      loop
+      muted
+      playsInline
+      src="https://pratichiera.com/wp-content/uploads/2024/09/video_09_2024.mp4"
+    />
+  );
+}
